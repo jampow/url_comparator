c4066515a60e67bbc105cc8d3b64b47a9bb9fce0
Gianpaulo M. Soares <jam_pow@hotmail.com>
2013-02-05 11:12:42 -0200

    waiting the load to execute js

--- script.js
+++ script.js
@@ -1,44 +1,43 @@
-$('#compare').click(function(){
-  
-  
-  var url1 = $("#url1").val();
-  var url2 = $("#url2").val();
-  
-  var url1par = parseUri(url1);
-  var url2par = parseUri(url2);
-  
-  var url1res = $('#url1res');
-  var url2res = $('#url2res');
-  
-  url1res.children().remove();
-  url2res.children().remove();
-  
-  compareLists(url1par, url2par, url1res);
-  compareLists(url2par, url1par, url2res);
-  
+$(function(){
+	$('#compare').click(function(){
+
+	  var url1 = $("#url1").val();
+	  var url2 = $("#url2").val();
+
+	  var url1par = parseUri(url1);
+	  var url2par = parseUri(url2);
+
+	  var url1res = $('#url1res');
+	  var url2res = $('#url2res');
+
+	  url1res.children().remove();
+	  url2res.children().remove();
+
+	  compareLists(url1par, url2par, url1res);
+	  compareLists(url2par, url1par, url2res);
+
+	});
 });
 
 function compareLists(parse1, parse2, result) {
   $.each(parse1.queryKey, function(k1,v1){
     var klass = "notfound";
-    
+
     $.each(parse2.queryKey, function(k2,v2){
       if(k1 == k2){
         // key founded
         klass = "key";
-        
+
         if(v1 == v2){
           // key and value equal
-          klass = "keyvalue";
+          klass += "value";
         }
-        
+
         return false;
-      } else {
-        // not found
       }
     });
-    
+
     result.append('<li class="'+klass+'">'+k1+"="+v1+'</li>');
-    
+
   });
 }
