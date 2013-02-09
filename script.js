69c1cd0c604c8c99be92f73ecb68dc90be46ec29
Gianpaulo M. Soares <jam_pow@hotmail.com>
2013-02-09 05:57:25 -0200

    adding highlight on sibling

--- script.js
+++ script.js
@@ -1,22 +1,38 @@
 $(function(){
-	$('#url1,#url2').keyup(function(){
+    $('#url1,#url2').keyup(function(){
 
-	  var url1 = $("#url1").val();
-	  var url2 = $("#url2").val();
+      var url1 = $("#url1").val();
+      var url2 = $("#url2").val();
 
-	  var url1par = parseUri(url1);
-	  var url2par = parseUri(url2);
+      var url1par = parseUri(url1);
+      var url2par = parseUri(url2);
 
-	  var url1res = $('#url1res');
-	  var url2res = $('#url2res');
+      var url1res = $('#url1res');
+      var url2res = $('#url2res');
 
-	  url1res.children().remove();
-	  url2res.children().remove();
+      url1res.children().remove();
+      url2res.children().remove();
 
-	  compareLists(url1par, url2par, url1res);
-	  compareLists(url2par, url1par, url2res);
+      compareLists(url1par, url2par, url1res);
+      compareLists(url2par, url1par, url2res);
 
-	});
+    });
+
+    $(document).on('mouseenter','ul li', function(){
+        var thisParent   = $(this).parent();
+        var thisParentId = thisParent.attr('id');
+
+        var other = thisParentId === 'url1res' ? 'url2res' : 'url1res';
+        var classes = $(this).text().split(' = ');
+
+        $(this).addClass('hover');
+        $('#'+other+' .'+classes[0]+'.'+classes[0]).addClass('hover');
+    });
+
+    $(document).on('mouseleave','ul li', function(){
+        $('ul li.hover').removeClass('hover');
+    });
+
 });
 
 function compareLists(parse1, parse2, result) {
@@ -37,7 +53,7 @@ function compareLists(parse1, parse2, result) {
       }
     });
 
-    result.append('<li class="'+klass+'">'+k1+"="+v1+'</li>');
+    result.append('<li class="'+klass+' '+k1+' '+v1+' '+'">'+k1+" = "+v1+'</li>');
 
   });
 }
