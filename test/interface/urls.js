96b368c39fbb4aaa6027153783cd0007a07b572a
Gianpaulo <jam_pow@hotmail.com>
2015-04-01 17:05:23 -0300

    adding mocha to the tests

--- test/interface/urls.js
+++ test/interface/urls.js
@@ -1,22 +1,36 @@
 var path = require('path');
 
-// Start with a webdriver instance:
 var sw = require('selenium-webdriver');
 var By = require('selenium-webdriver').By;
-var driver = new sw.Builder()
-  .withCapabilities(sw.Capabilities.chrome())
-  .build()
 
-// And then...
 var chai = require('chai');
 var chaiWebdriver = require('chai-webdriver');
-chai.use(chaiWebdriver(driver));
 
 var index = path.resolve(__dirname, '../../index.html');
 
-// And you're good to go!
-driver.get('file:///' + index);
-driver.findElement(By.id('url1')).sendKeys('http://sitedomain.com/?&test=1&param=2')
-chai.expect('#url1res').dom.to.contain.text("param = 2");
-// chai.expect('#url1res').dom.to.contain.text("test = 1");
-driver.quit();
+before(function(){
+	this.timeout(10000);
+	this.driver = new sw.Builder().withCapabilities(sw.Capabilities.chrome()).build();
+	chai.use(chaiWebdriver(this.driver));
+	return this.driver.getWindowHandle();
+})
+
+after(function() {
+  return this.driver.quit();
+});
+
+describe('Testing parameters',function(){
+	beforeEach(function() {
+		this.driver.get('file:///' + index);
+		return this.driver.findElement(By.id('url1')).sendKeys('http://sitedomain.com/?&test=1&param=2');
+	});
+
+	it('should have parameter named param with value 2',function(){
+		return chai.expect('#url1res').dom.to.contain.text("param = 2");
+	});
+
+	it('should have parameter named test with value 1',function(done){
+		return chai.expect('#url1res').dom.to.contain.text("merda");
+	});
+
+});
